43bc0a36d6387e70273096a213997d52abb342b4
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-06-22 23:25:00 +0530

    feat(Product Module) : create Product Table

--- resources/js/pages/Products/Create.tsx
+++ resources/js/pages/Products/Create.tsx
@@ -1,11 +1,12 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head,Link } from '@inertiajs/react';
+import { Head,Link, useForm } from '@inertiajs/react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import React from 'react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,22 +16,34 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Create() {
+
+    const {data,setData,post,processing,errors} = useForm({
+        name:'',
+        price:'',
+        description:'',
+    });
+
+    const handleSubmit = (e:React.FormEvent) =>{
+        e.preventDefault();
+        post(route('produts.store'));
+    }
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Products" />
             <div className="w-8/12 p-4">
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <div className="gap-1.5">
                         <Label htmlFor="productName">Name</Label>
-                        <Input id='productName' placeholder='Enter Product Name'></Input>
+                        <Input id='productName' value={data.name} onChange={(e)=>setData('name',e.target.value)} placeholder='Enter Product Name'></Input>
                     </div>
                     <div className="gap-1.5">
                         <Label htmlFor="productPrice">Price</Label>
-                        <Input id='productPrice' placeholder='Enter Product Price'></Input>
+                        <Input id='productPrice' value={data.price} onChange={(e)=>setData('price',e.target.value)}  placeholder='Enter Product Price'></Input>
                     </div> 
                     <div className="gap-1.5">
                         <Label htmlFor="productDescription">Description</Label>
-                        <Textarea id='productDescription' placeholder='description' />
+                        <Textarea id='productDescription' value={data.description} onChange={(e)=>setData('description',e.target.value)} placeholder='description' />
                     </div>
                     <Button>Add Product</Button>
                 </form>
