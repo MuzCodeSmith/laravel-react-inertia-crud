ee7f40cda91795387c33b29f550b19132322b5d9
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-06-24 07:57:04 +0530

    feat(Product Module) : display flash messge on Product creation

--- resources/js/pages/Products/Index.tsx
+++ resources/js/pages/Products/Index.tsx
@@ -2,7 +2,9 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Button } from '@/components/ui/button';
-import { Head,Link } from '@inertiajs/react';
+import { Head,Link, usePage } from '@inertiajs/react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Megaphone } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,12 +13,27 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+interface PageProps {
+    flash:{
+        message?:string
+    }
+}
+
 export default function Index() {
+    const {flash} = usePage().props as PageProps;
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Products" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <Link href={route('products.create')}><Button>Create Product</Button></Link>
+                {/* Flash Message */}
+                {flash.message && (
+                    <Alert className="mb-4">
+                        <Megaphone className="h-4 w-4" />
+                        <AlertTitle>Notification</AlertTitle>
+                        <AlertDescription>{flash.message}</AlertDescription>
+                    </Alert>
+                )}
             </div>
         </AppLayout>
     );
